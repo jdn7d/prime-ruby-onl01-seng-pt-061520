573e864284383a3514fb31763893618ca3bc0496
Jessie Donovan <donovanajessie@gmail.com>
2020-06-27 16:59:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,12 @@ def prime?(num)
   if num <= 1
     return false
   end
-  (2..num).to_a.each do |number|
+  (2..num-1).to_a.each do |number|
   if num % number == 0
-    return true
+    return false
 
   else
-    return false
+    return true
   end
 end
 end
