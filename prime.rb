50fbafadd86e94f852f6924e6609faa144bfcb74
Jessie Donovan <donovanajessie@gmail.com>
2020-06-27 17:13:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require "pry"
+def prime?(num)
+
+  if num <= 1
+    return false
+  end
+  (2..num-1).to_a.each do |number|
+    if num % number == 0
+      binding.pry
+    return false
+
+    end
+  end
+    return true
+end
