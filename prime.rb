9b6f4dbbdbcae57cea476c48a7658feee5ab6172
Jessie Donovan <donovanajessie@gmail.com>
2020-06-27 17:04:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+require "pry"
 def prime?(num)
 
   if num <= 1
@@ -5,7 +6,9 @@ def prime?(num)
   end
   (2..num-1).to_a.each do |number|
     if num % number == 0
+      binding.pry
     return false
+    
     end
   end
     return true
