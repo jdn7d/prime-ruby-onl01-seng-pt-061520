dd243dc13a5bd8da8af72f29d2e58ddd6b9a0a9e
Jessie Donovan <donovanajessie@gmail.com>
2020-06-27 16:39:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 def prime?(num)
   (1...num).each do |number|
-  if num % number == 0
-    return true
-elsif
-  num < 0
+  if   num < 0
   return false
-
+elsif
+  num % number == 0
+    return true
 else
     return false
   end
