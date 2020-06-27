e71dd54f0bf2bc82583599ff28b546bc29df46ed
Jessie Donovan <donovanajessie@gmail.com>
2020-06-27 16:12:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(num)
   prime_array = []
 
-(1...x).each do |num|
-  if (1..num).select { |number| num % d == 0 } == [1, num]
+(1...x).each do |number|
+
+  return false if num % number == 0
+  
+  if (1..num).select { |number| num % number == 0 }
     prime_array.push(num)
   end
 end
